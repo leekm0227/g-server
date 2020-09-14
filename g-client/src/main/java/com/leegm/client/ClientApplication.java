4e1fdcec156adf8883d4fb965530812ac9b2eb36
leegm <leekm0227@naver.com>
2020-09-15 00:02:10 +0900

    init

---
+++ g-client/src/main/java/com/leegm/client/ClientApplication.java
@@ -0,0 +1,14 @@
+package com.leegm.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+}
