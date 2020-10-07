28c439382d9f46fc4749f1710cad5609e3748faa
leegm <leekm0227@naver.com>
2020-10-08 00:53:59 +0900

    init netty

--- g-channel/src/main/java/com/leegm/channel/ChannelApplication.java
+++ g-channel/src/main/java/com/leegm/channel/ChannelApplication.java
@@ -1,27 +1,8 @@
 package com.leegm.channel;
 
-import com.leegm.common.util.Dispatcher;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
-
-@SpringBootApplication
-@EnableScheduling
 public class ChannelApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ChannelApplication.class, args);
-    }
 
-    @Bean
-    public Dispatcher dispatcher() {
-        return new Dispatcher();
-    }
-
-    @Bean
-    public WebSocketHandlerAdapter handlerAdapter() {
-        return new WebSocketHandlerAdapter();
     }
 }
